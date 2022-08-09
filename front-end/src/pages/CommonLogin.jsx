466a64495b6cc2d29a963885bb2f9c0e627800a4
Antonio Sabino <aslimajr@outlook.com>
2022-08-09 15:43:11 -0300

    fix: ajuste lint

--- front-end/src/pages/CommonLogin.jsx
+++ front-end/src/pages/CommonLogin.jsx
@@ -13,23 +13,29 @@ export default function CommonLogin() {
   return (
     <div className="login-container">
       <form className="login-form">
-        <label>Login</label>
-        <input
-          type="text"
-          name="login"
-          data-testid="common_login__input-email"
-          onChange={ (e) => setInputs({ ...inputs, email: e.target.value }) }
-          value={ inputs.email }
-        />
-        <label>Senha</label>
-        <input
-          type="password"
-          name="senha"
-          data-testid="common_login__input-password"
-          onChange={ (e) => setInputs({ ...inputs, password: e.target.value }) }
-          value={ inputs.password }
-        />
-        <button data-testid="common_login__button-login">Login</button>
+        <label htmlFor="login">
+          Login
+          <input
+            type="text"
+            name="login"
+            data-testid="common_login__input-email"
+            onChange={ (e) => setInputs({ ...inputs, email: e.target.value }) }
+            value={ inputs.email }
+          />
+        </label>
+        <label htmlFor="senha">
+          Senha
+          <input
+            type="password"
+            name="senha"
+            data-testid="common_login__input-password"
+            onChange={ (e) => setInputs({ ...inputs, password: e.target.value }) }
+            value={ inputs.password }
+          />
+        </label>
+        <button data-testid="common_login__button-login" type="button">
+          Login
+        </button>
         <button
           data-testid="common_login__button-register"
           type="button"
