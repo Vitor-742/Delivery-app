09caa2a297f05ae81d8939c40c752a49d4d25a22
Antonio Sabino <aslimajr@outlook.com>
2022-08-09 15:38:23 -0300

    fix: ajuste pages Commonlogin para requisito CommonRegister

--- front-end/src/pages/CommonLogin.jsx
+++ front-end/src/pages/CommonLogin.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function CommonLogin() {
-  const [inputs, setInputs] = useState({ email: "", password: "" });
+  const [inputs, setInputs] = useState({ email: '', password: '' });
+
+  const history = useHistory();
+
+  function handleClick() {
+    history.push('/register');
+  }
 
   return (
     <div className="login-container">
@@ -11,19 +18,23 @@ export default function CommonLogin() {
           type="text"
           name="login"
           data-testid="common_login__input-email"
-          onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-          value={inputs.email}
+          onChange={ (e) => setInputs({ ...inputs, email: e.target.value }) }
+          value={ inputs.email }
         />
         <label>Senha</label>
         <input
           type="password"
           name="senha"
           data-testid="common_login__input-password"
-          onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-          value={inputs.password}
+          onChange={ (e) => setInputs({ ...inputs, password: e.target.value }) }
+          value={ inputs.password }
         />
         <button data-testid="common_login__button-login">Login</button>
-        <button data-testid="common_login__button-register">
+        <button
+          data-testid="common_login__button-register"
+          type="button"
+          onClick={ handleClick }
+        >
           Ainda não tenho conta
         </button>
         <span data-testid="common_login__element-invalid-email" />
