e3e6b520a53e5aab0853d764058c19400000aafc
Antonio Sabino <aslimajr@outlook.com>
2022-08-09 18:57:07 -0300

    fix: ajuste registerValidate

--- front-end/src/utils/registerValidate.js
+++ front-end/src/utils/registerValidate.js
@@ -1,8 +1,4 @@
 const registerValidate = (name, email, password) => {
-  /**
-   * Validação com Regex consultada em
-   * https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
-   */
   const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
   const NAME_LENGTH = 12;
   const PASSWORD_LENGTH = 6;
