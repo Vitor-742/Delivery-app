4a6fd8d9f128d15600dfe1479986280a6653d406
lkotlarenko <lkotlarenko@protonmail.com>
2022-08-10 21:34:06 -0300

    feat(frontend): ✨ new page 'Products'
    add:
     page 'customer/products' with 'NavBar'.

---
+++ front-end/src/pages/customer/Products.jsx
@@ -0,0 +1,7 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+import NavBar from '../../components/NavBar/NavBar';
+
+export default function Products() {
+  return <NavBar />;
+}
