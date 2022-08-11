31d41f672794265ea0798dcf5b66184c3f235fb3
lkotlarenko <lkotlarenko@protonmail.com>
2022-08-11 20:02:14 -0300

    fix(backend): 🚨 lint error

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -9,7 +9,7 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static(__dirname + '/../../public/images'))
+app.use('/images', express.static(`${__dirname}/../../public/images`));
 app.use(userRouter);
 app.use(customerRouter);
 app.use(errorHandler);
