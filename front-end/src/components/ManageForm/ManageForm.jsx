9fa940ba6f09d1bc362e568c35bb8468189e9fdf
Caio Oliveira <caiodevoliveira@gmail.com>
2022-08-12 17:31:58 -0300

    Validações

--- front-end/src/components/ManageForm/ManageForm.jsx
+++ front-end/src/components/ManageForm/ManageForm.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
+const MIN_LENGTH_PASSWORD = 5;
+const MIN_LENGTH_NAME = 11;
 
 export default function ManageForm() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "customer",
+  });
+  const [disabledRegister, setDisableRegister] = useState(false);
+  const [incorrectLogin, setIncorrectLogin] = useState(false);
+
+  const ableBtnRegister = () => {
+    const reg = /\S+@\S+\.\S+/;
+    if (
+      reg.test(inputs.email) &&
+      inputs.password.length > MIN_LENGTH_PASSWORD &&
+      inputs.name.length > MIN_LENGTH_NAME
+    ) {
+      setDisableRegister(true);
+    } else {
+      setDisableRegister(false);
+    }
+  };
+
+  const Register = async () => {
+    try {
+    } catch (error) {}
+  };
+
   return (
     <div>
       <label htmlFor="name">
         Nome
-        <input type="text" name="name" data-testid="admin_manage__input-name" />
+        <input
+          type="text"
+          name="name"
+          data-testid="admin_manage__input-name"
+          onChange={(e) => {
+            setInputs({ ...inputs, name: e.target.value });
+            ableBtnRegister();
+          }}
+          value={inputs.name}
+        />
       </label>
       <label htmlFor="email">
         Email
@@ -13,6 +52,11 @@ export default function ManageForm() {
           type="text"
           name="email"
           data-testid="admin_manage__input-email"
+          onChange={(e) => {
+            setInputs({ ...inputs, email: e.target.value });
+            ableBtnRegister();
+          }}
+          value={inputs.email}
         />
       </label>
       <label htmlFor="password">
@@ -21,12 +65,25 @@ export default function ManageForm() {
           type="password"
           name="password"
           data-testid="admin_manage__input-password"
+          onChange={(e) => {
+            setInputs({ ...inputs, password: e.target.value });
+            ableBtnRegister();
+          }}
+          value={inputs.password}
         />
       </label>
       <label htmlFor="role">
         Tipo
-        <select name="role" data-testid="admin_manage__select-role">
-          <option value="client" name="client">
+        <select
+          name="role"
+          data-testid="admin_manage__select-role"
+          onChange={(e) => {
+            setInputs({ ...inputs, role: e.target.value });
+            ableBtnRegister();
+          }}
+          value={inputs.role}
+        >
+          <option value="customer" name="customer">
             Cliente
           </option>
           <option value="seller" name="seller">
@@ -34,7 +91,13 @@ export default function ManageForm() {
           </option>
         </select>
       </label>
-      <button data-testid="admin_manage__button-register">Cadastrar</button>
+      <button
+        data-testid="admin_manage__button-register"
+        disabled={!disabledRegister}
+        onClick={() => Register()}
+      >
+        Cadastrar
+      </button>
     </div>
   );
 }
