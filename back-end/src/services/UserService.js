de951d551efc334e07d97607e99d44f5254d2a44
Vitor Campos <vitorcampos742@gmail.com>
2022-08-15 20:54:36 -0300

    lint

--- back-end/src/services/UserService.js
+++ back-end/src/services/UserService.js
@@ -1,18 +1,22 @@
 const md5 = require('md5');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const jwtKey = require('fs')
+  .readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
 const newJWT = require('../auth/tokenHandler');
 const { Users } = require('../database/models');
 const errorCreator = require('../helpers/errorCreator');
-const jwtKey = require("fs")
-  .readFileSync("./jwt.evaluation.key", { encoding: "utf-8" });
 
 const login = async ({ email, password: unCryptPass }) => {
   const password = md5(unCryptPass);
   const loginUser = await Users.findOne({ where: { email, password } });
   if (loginUser) {
-    const {name, role} = loginUser
-    const secret = jwtKey    
-    loginUser.dataValues.token = jwt.sign({name, email, role}, secret, { algorithm: 'HS256', expiresIn: '7d' });
+    const { name, role } = loginUser;
+    const secret = jwtKey;    
+    loginUser.dataValues.token = jwt.sign(
+      { name, email, role },
+      secret,
+      { algorithm: 'HS256', expiresIn: '7d' },
+    );
     return loginUser;
   }
   throw errorCreator(404, 'User Not Found');
