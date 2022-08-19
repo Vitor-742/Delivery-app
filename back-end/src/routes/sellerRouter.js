9e02d20adfa1bb5fae7258208db958135705a265
lkotlarenko <lkotlarenko@protonmail.com>
2022-08-18 23:33:27 -0300

    feat(backend): ✨ add new endpoints to 'sellerRouter'
    add:
     'GET' endpoint '/seller/orders/:userId' to get all orders with param 'userId'.
     'GET' endpoint '/seller/orders/:sellerId' to get all orders with param 'sellerId'.

--- back-end/src/routes/sellerRouter.js
+++ back-end/src/routes/sellerRouter.js
@@ -4,5 +4,10 @@ const SellerController = require('../controllers/SellerController');
 const sellerRouter = express.Router();
 
 sellerRouter.get('/seller/orders', SellerController.getSales);
+sellerRouter.get('/seller/orders/:userId', SellerController.getSalesByUserId);
+sellerRouter.get(
+  '/seller/orders/:sellerId',
+  SellerController.getSalesBySellerId,
+);
 
-module.exports = sellerRouter; 
+module.exports = sellerRouter;
