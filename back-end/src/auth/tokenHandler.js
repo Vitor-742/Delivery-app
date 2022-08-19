62b1172b7e06a8c6bc5797496eba120a8494065d
lkotlarenko <lkotlarenko@protonmail.com>
2022-08-19 00:57:30 -0300

    fix(backend): 🐛 use 'jwt.evaluation.key' as 'JWT_SECRET'
    change:
     update 'newJWT' to create tokens using 'JWT_SECRET' from 'jwt.evaluation.key'.

--- back-end/src/auth/tokenHandler.js
+++ back-end/src/auth/tokenHandler.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'secret_key';
+const JWT_SECRET = fs.readFileSync('jwt.evaluation.key', 'utf-8')
+  || process.env.JWT_SECRET
+  || 'secret_key';
 
 const newJWT = (payload) => {
   const newToken = jwt.sign({ data: payload }, JWT_SECRET, {
