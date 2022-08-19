ce6cdcaa4d7b11b392de30ea6be3d6049c3f8374
lkotlarenko <lkotlarenko@protonmail.com>
2022-08-19 01:00:06 -0300

    feat(backend): 🛂 add tokenAuth middleware to 'admin/manage'

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { tokenAuth } = require('../auth/tokenAuth');
 const UserController = require('../controllers/UserController');
 const userValidator = require('../middlewares/userValidator');
 
@@ -6,6 +7,6 @@ const userRouter = express.Router();
 
 userRouter.post('/login', UserController.login);
 userRouter.post('/register', userValidator, UserController.signUp);
-userRouter.post('/admin/manage', userValidator, UserController.adminRegister);
+userRouter.post('/admin/manage', tokenAuth, userValidator, UserController.adminRegister);
 
 module.exports = userRouter;
