3a573f14301f6334d0961f1eac687d7a62e69660
lkotlarenko <lkotlarenko@protonmail.com>
2022-08-19 00:56:11 -0300

    feat(backend): 🛂 new middleware 'tokenAuth'
    add:
     middleware to validate token from headers.authorization.

---
+++ back-end/src/auth/tokenAuth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const JWT_SECRET = fs.readFileSync('jwt.evaluation.key', 'utf-8')
+  || process.env.JWT_SECRET
+  || 'secret_key';
+
+const tokenAuth = (req, _res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      next({ status: 401, message: 'Token not found' });
+    }
+    jwt.verify(token, JWT_SECRET);
+    next();
+  } catch (err) {
+    next({ status: 401, message: 'Token must be a valid' });
+  }
+};
+
+module.exports = { tokenAuth };
